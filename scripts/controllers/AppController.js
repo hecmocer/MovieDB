1dc9fd9f53f6bc9f59c6688ce8324fd13ab1aeb1
Héctor <hecmocer@gmail.com>
2016-03-22 09:40:06 +0100

    El título de la página se recarga cuando cambiamos de pestaña
    Incluimos la carpeta dist en el gitignore

--- scripts/controllers/AppController.js
+++ scripts/controllers/AppController.js
@@ -1,17 +1,25 @@
 angular
 .module("moviedb")
 .controller("AppController",
-    ["$scope", function($scope){
+    ["$scope", "$location", function($scope, $location){
+        var controller = this;
+
+        // Controller properties
+        controller.titles = {
+            "/movies": "Movies List",
+            "/series": "Series List",
+            "/people": "People List"
+        };
 
         // Model init
         $scope.model = {
             title: ""
-        }
+        };
 
         // Scope event listeners
         // Recibimos evento de cambio de menu
-        $scope.$on("OnMenuChange", function(evt, data){
-            $scope.model.title = data;
+        $scope.$on("$locationChangeSuccess", function(evt, currentRoute){
+            $scope.model.title = controller.titles[$location.path()] || "404 Not Found";
         });
 
     }]
